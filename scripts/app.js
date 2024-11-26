fa931404017051a9c1af2ab8e4683c6661daa59b
HankChau-AI <amorandrie5@gmail.com>
2024-11-26 14:54:50 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,78 @@
+// We will pull the data from the JSON file and randomly select a student 
+// Create a function that then randomize a selection from that data
+// On a button click we will display the data onto the DOM
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let randomName = document.getElementById("randomName")
+let fullName = document.getElementById("fullName")
+let previousNames = [];
+
+// Create a function that will pull the data from the JSON file
+// .then() is a promise that will wait for the data to be returned
+// promise can be accepted or rejected
+function getStudentData(){
+    return fetch('../data/data.json')
+    .then( response => response.json())
+    .then(data => {
+        console.log(data)
+        return data.students;
+    })
+}
+
+getStudentData();
+//Create a function that will randomize the selection of a student
+function getRandomStudent(students){
+    let randomIndex = Math.floor(Math.random() * students.length);
+    console.log([randomIndex]);
+    return students[randomIndex];
+}
+
+//Create a function that will display the data on to the DOM
+randomName.addEventListener('click', () =>{
+    getStudentData().then( students => {
+        // let randomStudent = getRandomStudent(students);
+        // console.log(randomStudent);
+        // firstName.innerText = randomStudent.firstName;
+        // lastName.innerText = randomStudent.lastName;
+        
+        let randomStudent = getRandomStudent(students);
+        console.log(randomStudent);
+        let fullName = randomStudent.firstName + " " + randomStudent.lastName;
+        fullNameElement.innerText = fullName;
+        previousNames.push(fullName);
+        nameList();
+
+            
+    })
+});
+
+function nameList(){
+    if (previousNames.length > 5) {
+      previousNames.shift(); // Remove the oldest name
+    }
+  
+    const previousNamesList = document.getElementById('previousNamesList');
+    previousNamesList.innerHTML = ''; // Clear the list
+  
+    previousNames.forEach(name => {
+      const li = document.createElement('li');
+      li.textContent = name;
+      previousNamesList.appendChild(li);
+    });
+  }
+
+// function saveAndDisplayNames(fullNamename) {
+//     previousNames.push(fullNamename);
+//     if (previousNames.length > 5) {
+//       previousNames.shift();
+//     }
+  
+//     const previousNamesList = document.getElementById('previousNamesList');
+//     previousNamesList.innerHTML = '';
+//     previousNames.forEach(fullNamename => {
+//       const li = document.createElement('li');
+//       li.textContent = fullName;
+//       previousNamesList.appendChild(li);
+//     });
+//   }
